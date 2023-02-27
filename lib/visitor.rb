b861ce6f6a8e1ee4277c2f971ab60e8b764c7310
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-02-27 09:42:17 -0700

    Feat: add methods to visitor class.

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,3 +1,26 @@
 class Visitor
-  
+  attr_reader :name, :height, :spending_money, :preferences
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+  def spending_money
+    @spending_money.slice!(0)
+    @spending_money.to_i
+  end
+
+  def add_preferences(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(min_height_limit)
+    if @height >= min_height_limit
+      true
+    else
+      false
+    end
+  end
 end
